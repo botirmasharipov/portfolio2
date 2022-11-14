99561ce7e074e28a8e8b01887638f2c74816bf96
botirmasharipov <masbotirjon963@gmail.com>
2022-11-14 10:29:32 -0800

    aboutMePage

---
+++ about-me-page/app.js
@@ -0,0 +1,24 @@
+main-heading{
+    animation: 10000ms ease-in-out infinite color-change;
+  }
+  
+  @keyframes color-change {
+    0% {
+      color: teal;
+    }
+    20% {
+      color: gold;
+    }
+    40% {
+      color: indianred;
+    }
+    60% {
+      color: violet;
+    }
+    80% {
+      color: green;
+    }
+    100% {
+      color: teal;
+    }
+  }
